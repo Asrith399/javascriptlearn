88cfad8fae1b72913f86eea1f00461a8ce3d1df1
asrith399 <asrith001@hotmail.com>
2024-11-17 22:16:30 +0530

    js learn

---
+++ code.js
@@ -0,0 +1,5 @@
+
+    let name = "Asrith"
+    console.log("Variable Value showing in Browser inspect console linked to code")
+   console.log(name)
+
---
+++ logic.js
@@ -0,0 +1,10 @@
+let totalPrice = 20;
+
+if (totalPrice <= 10) {
+  shippingCost = 5
+
+}else{
+    shippingCost = 3
+}
+
+console.log(`your shipping cost is ${shippingCost}`)
